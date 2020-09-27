30cab9e599103114a452af8f7599a15d6cce8499
Aangimehta <aangimehta@hotmail.com>
2020-09-27 20:21:31 +0530

    Updates

---
+++ precache-manifest.093c6a034e724adb577c38c6bc29ad7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ca1f1211cb61efd5a4fa842ca1de5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8493327e7c8362c1a2b",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e6e3f3e3ffa3d9d50567",
+    "url": "/static/css/main.8786f6b7.chunk.css"
+  },
+  {
+    "revision": "b8493327e7c8362c1a2b",
+    "url": "/static/js/2.76e645c0.chunk.js"
+  },
+  {
+    "revision": "97557dac625cf0680a71c54f9bece515",
+    "url": "/static/js/2.76e645c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6e3f3e3ffa3d9d50567",
+    "url": "/static/js/main.2bc75b9c.chunk.js"
+  },
+  {
+    "revision": "59db69db05ebe7a9f7d7",
+    "url": "/static/js/runtime-main.557a9a42.js"
+  }
+]);
